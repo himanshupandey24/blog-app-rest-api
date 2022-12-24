de69f8ddd8cc84176130f56ea4c9e20a83c9c62b
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-12-24 18:20:50 +0530

    initial commit

---
+++ src/main/java/com/restapi/springbootrestapi/SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.springbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restapi/springbootrestapi/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
